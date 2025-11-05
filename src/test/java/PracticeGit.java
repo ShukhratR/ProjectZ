fb3f5d30e6192f0761dfea40f8e31a431edc8793
Zilola <rustamova.zilolahon@gmail.com>
2025-11-05 15:33:13 +0500

    added Scanner and greeting message

--- src/test/java/PracticeGit.java
+++ src/test/java/PracticeGit.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class PracticeGit {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello, World!");
+        System.out.println("What is your name ?");
+
+        String name = scanner.nextLine();
+        System.out.println("Nice to meet you, " + name + "!");
+
+        scanner.close();
     }
 }
+
