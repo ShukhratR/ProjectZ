936a0648f864dbe4c2340734f867086af921c8f7
Zilola <rustamova.zilolahon@gmail.com>
2025-11-06 22:28:11 +0500

    Add Selenium automation for Yahoo login

--- src/test/java/SeleniumDay1.java
+++ src/test/java/SeleniumDay1.java
@@ -5,25 +5,41 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDay1 {
-    public static void main(String[] args) {
-
-
-
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
+        driver.get("https://login.yahoo.com/");
+        Thread.sleep(2000);
+
+        WebDriverManager.chromedriver().setup();
+
 
         driver.get("https://login.yahoo.com/");
+        Thread.sleep(2000);
 
         // id:
         WebElement username = driver.findElement(By.id("login-username"));
         username.sendKeys("testZilola@yahoo");
+        Thread.sleep(2000);
 
         WebElement nextBtn = driver.findElement(By.id("login-signin"));
+
         nextBtn.click();
 
+        driver.findElement(By.id("login-username"));
+    }}
 
 
+
+      /* driver.findElement(By.name("username")).sendKeys("test@yahoo.com");
+        driver.findElement(By.name("signin")).click();
 
     }
-}
+    } */
+
+
+
+
+
+
