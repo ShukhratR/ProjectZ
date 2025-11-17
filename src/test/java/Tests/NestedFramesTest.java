804b0efffbdef1c006efe04223573c9afd867fb6
Zilola <rustamova.zilolahon@gmail.com>
2025-11-17 21:42:18 +0500

    Nested frame handling practice

---
+++ src/test/java/Tests/NestedFramesTest.java
@@ -0,0 +1,52 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.NestedFramesPage;
+
+public class NestedFramesTest {
+
+    private WebDriver driver;
+    private NestedFramesPage framesPage;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        framesPage = new NestedFramesPage(driver);
+        framesPage.open();
+    }
+
+    @Test
+    public void testLeftFrame() {
+        String text = framesPage.getLeftFrameText();
+        Assert.assertEquals(text, "LEFT");
+    }
+
+    @Test
+    public void testMiddleFrame() {
+        String text = framesPage.getMiddleFrameText();
+        Assert.assertEquals(text, "MIDDLE");
+    }
+
+    @Test
+    public void testRightFrame() {
+        String text = framesPage.getRightFrameText();
+        Assert.assertEquals(text, "RIGHT");
+    }
+
+    @Test
+    public void testBottomFrame() {
+        String text = framesPage.getBottomFrameText();
+        Assert.assertEquals(text, "BOTTOM");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/pages/NestedFramesPage.java
@@ -0,0 +1,57 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NestedFramesPage {
+
+
+        private final WebDriver driver;
+
+        private final By bodyText = By.tagName("body");
+        private final String topFrame = "frame-top";
+        private final String leftFrame = "frame-left";
+        private final String middleFrame = "frame-middle";
+        private final String rightFrame = "frame-right";
+        private final String bottomFrame = "frame-bottom";
+
+        public NestedFramesPage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public void open() {
+            driver.get("https://the-internet.herokuapp.com/nested_frames");
+        }
+
+        public String getLeftFrameText() {
+            driver.switchTo().frame(topFrame);
+            driver.switchTo().frame(leftFrame);
+            String text = driver.findElement(bodyText).getText();
+            driver.switchTo().defaultContent();
+            return text;
+        }
+
+        public String getMiddleFrameText() {
+            driver.switchTo().frame(topFrame);
+            driver.switchTo().frame(middleFrame);
+            String text = driver.findElement(By.id("content")).getText();
+            driver.switchTo().defaultContent();
+            return text;
+        }
+
+        public String getRightFrameText() {
+            driver.switchTo().frame(topFrame);
+            driver.switchTo().frame(rightFrame);
+            String text = driver.findElement(bodyText).getText();
+            driver.switchTo().defaultContent();
+            return text;
+        }
+
+        public String getBottomFrameText() {
+            driver.switchTo().frame(bottomFrame);
+            String text = driver.findElement(bodyText).getText();
+            driver.switchTo().defaultContent();
+            return text;
+        }
+    }
+
